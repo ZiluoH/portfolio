64bd480eda77be080e4217f040f2a22892fca205
ZiluoH <ziluoh@gmail.com>
2017-06-20 18:02:48 -0700

    Add files via upload

---
+++ Flowers-site/js/main.js
@@ -0,0 +1,68 @@
+// setup array data
+var flowersArray = []
+
+function flower(img, name, price, stock) {
+	this.img = img
+	this.name = name
+	this.price = price
+	this.stock = stock
+	flowersArray.push(this)
+}
+
+// flowers data that show in flowersArray
+var 
+
+
+
+
+// var toyota_chr = new Car('img/chr.jpg','CHR','Toyota',2017,28000,12)
+
+// // for (var i = 0; i < focus.length; i++) {
+// // 	console.log(focus[i])
+// // }
+
+
+
+// for (var i = 0; i < carArray.length; i++) {
+// 	var carImg = document.createElement('img')
+// 	var carName = document.createElement('h2')
+//     var carMake = document.createElement('p')
+//     var carYear = document.createElement('p')
+//     var carPrice = document.createElement('p')
+//     var button = document.createElement('button')
+    
+    
+//     if (carArray[i].stock > 5) {
+//          button.innerHTML = ("buy me")  
+//     } else if (carArray[i].stock > 0 ) {
+//     	button.innerHTML = ("only " + carArray[i].stock + " left")
+//     } else{
+//     	button.innerHTML = ("out of stock")
+        
+//     }
+
+//     var car = document.createElement('div')
+
+//     carMake.innerHTML = (carArray[i].make)
+//     carYear.innerHTML = (carArray[i].year)
+//     carPrice.innerHTML = (carArray[i].price)
+//     carName.innerHTML = (carArray[i].name)
+    
+//     carImg.src = (carArray[i].img)
+//     carImg.className = 'img-responsive'
+//     carImg.style.maxHeight = "200px"
+
+//     car.appendChild(carImg)
+//     car.appendChild(carName)
+//     car.appendChild(carMake)
+//     car.appendChild(carYear)
+//     car.appendChild(carPrice)
+//     car.appendChild(button)
+//     car.className = "col-md-6"
+//     car.className += " carclass"
+    
+//     document.getElementById('car').appendChild(car)
+
+//     // car.addClass("col-md-4")
+
+// }
